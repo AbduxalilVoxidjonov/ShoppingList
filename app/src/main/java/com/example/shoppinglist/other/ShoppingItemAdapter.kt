b6aa653809cf4d23470912550502e082a35ba602
AbduxalilVoxidjonov <abduhalilvohidjonov@gmail.com>
2024-02-18 22:42:26 +0500

    Second Commit

---
+++ app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.shoppinglist.other
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+import com.example.shoppinglist.data.db.entity.ShoppingItem
+import com.example.shoppinglist.databinding.ShoppingItemBinding
+import com.example.shoppinglist.ui.shoppinglist.ShoppingViewModel
+
+// recycle view adapter in binding settings
+class ShoppingItemAdapter(
+    var items: List<ShoppingItem>,
+    private val viewModel: ShoppingViewModel
+) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val binding =
+            ShoppingItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ShoppingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val curShoppingItem = items[position]
+        holder.binding.apply {
+            tvName.text = curShoppingItem.name
+            tvAmount.text = "${curShoppingItem.amount}"
+
+            ivDelete.setOnClickListener {
+                viewModel.delete(curShoppingItem)
+            }
+            ivPlus.setOnClickListener {
+                curShoppingItem.amount++
+                viewModel.upsert(curShoppingItem)
+            }
+            ivMinus.setOnClickListener {
+                if (curShoppingItem.amount > 0) {
+                    curShoppingItem.amount--
+                    viewModel.upsert(curShoppingItem)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ShoppingViewHolder(val binding: ShoppingItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -7,16 +7,23 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.ShoppingDatabase
 import com.example.shoppinglist.data.repository.ShoppingRepository
+import com.example.shoppinglist.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
 
         val database = ShoppingDatabase(this)
         val repository = ShoppingRepository(database)
         val factory = ShoppingViewModelFactory(repository)
 
-        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this, factory)[ShoppingViewModel::class.java]
+
+
     }
 }
